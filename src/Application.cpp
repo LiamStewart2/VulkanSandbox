febf82d441bf3562133229a8c79503e48ac9e9c9
Liam Stewart <117774269+LiamStewart2@users.noreply.github.com>
2026-06-08 12:54:35 +0100

    setup initial vulkan application

--- src/Application.cpp
+++ src/Application.cpp
@@ -2,14 +2,100 @@
 
 Application::Application() 
 {
-
+    Init();
+    Mainloop();
+    Cleanup();
 }
 Application::~Application() 
 {
     
 }
 
+void Application::Mainloop()
+{
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+}
+
+void Application::Init()
+{
+    InitWindow();
+    InitVulkan();
+}
+
+void Application::InitWindow()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // prevents glfw making an OpenGL context
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Sandbox", nullptr, nullptr);
+
+
+}
+
+void Application::InitVulkan()
+{
+    CreateInstance();
+}
+
+
+
+void Application::CreateInstance()
+{
+    /* Definition for the application - technically optional */
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Vulkan Sandbox";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "Luna Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    /* specifies how the VkInstance should be created */
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    createInfo.enabledLayerCount = 0;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create instance!");
+    }
+
+
+
+}
+
+
+void Application::HandleEvents()
+{
+}
+
 void Application::Update()
 {
     std::cout << "Updating application..." << std::endl;
+}
+
+void Application::Render()
+{
+}
+
+void Application::Cleanup()
+{
+    vkDestroyInstance(instance, nullptr);
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
--- src/Application.h
+++ src/Application.h
@@ -1,5 +1,14 @@
 #pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+#include <GLFW/glfw3.h>
+#include <GLM/glm.hpp>
 
 class Application
 {
@@ -7,5 +16,26 @@ public:
     Application();
     ~Application();
 
+private:
+    void Mainloop();
+
+    void Init();
+    void InitWindow();
+    void InitVulkan();
+    
+    void CreateInstance();
+    
+
+    void HandleEvents();
     void Update();
+    void Render();
+
+    void Cleanup();
+
+private:
+    GLFWwindow* window;
+    VkInstance instance;
+
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
 };
